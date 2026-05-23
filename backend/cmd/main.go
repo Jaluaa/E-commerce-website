ef92e3a9c940163466993623ae25c61729abdc22
Jalwa <jalwajabbar@gmail.com>
2026-05-24 00:14:01 +0530

    ready to deploy

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -129,8 +129,14 @@ func main() {
 	r := gin.Default()
 
 	// CORS Setup
+	frontendURL := os.Getenv("FRONTEND_URL")
+	allowedOrigins := []string{"http://localhost:5173", "http://localhost:3000"}
+	if frontendURL != "" {
+		allowedOrigins = append(allowedOrigins, frontendURL)
+	}
+
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowOrigins:     allowedOrigins,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
